5f468a0a45c5e40ba7928406f67fb2544b2250fe
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-02-22 15:53:47 -0300

    Ajustes na impressão de dados de previsão semanal

--- pagina.php
+++ pagina.php
@@ -3,9 +3,8 @@
 	<head>
 	 	<meta charset="UTF-8">
 		<title>Previsão para <?php echo $p["nome"]; ?> - CPTEC/INPE</title>
-		<link rel="stylesheet" type="text/css" href="estilo.css">
 	</head>
-	<body>
+	<body style="font-family:sans-serif;">
 		<header>
 			<h1><?php echo $p["nome"]; ?> - <?php echo $p["uf"]; ?></h1>
 		</header>
@@ -15,6 +14,7 @@
 		<p><?php echo $ps["desc"]; ?></p>
 		<p><?php echo $ps["texto"]; ?></p>
 		<p>Temperaturas: Min: <?php echo $ps["min"]; ?> - Max: <?php echo $ps["max"]; ?></p>
+		<p>Probabilidade de chuva: <?php echo $ps["prob"]; ?></p>
 		<p>Nascer do Sol: <?php echo $ps["sunrise"]; ?> - Pôr do Sol: <?php echo $ps["sunset"]; ?></p>
 		<p>UV: <?php echo $ps["uv"]; ?></p>
 		<?php } ?>		
@@ -23,4 +23,4 @@
 		</footer>
 	<!-- ANALYTICS -->
 	</body>
-</html> 
+</html>
