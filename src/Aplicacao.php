8f087aa19541c9aad96a772c59b30665177df857
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-03-02 15:27:24 -0300

    Inclusão do parâmetro de tipo de página

--- src/Aplicacao.php
+++ src/Aplicacao.php
@@ -6,17 +6,28 @@ class Aplicacao{
 		$erros = array();
 		
 		$cl = isset($_GET['cidade']) ? $_GET['cidade'] : NULL;
+		$tp = isset($_GET['pagina']) ? $_GET['pagina'] : NULL;
 		if(is_null($cl) || empty($cl)){
 			$erros[] = "Nenhuma cidade foi informada";
 			require_once("templates/erro_generico.php");
 			die();
 		}
+
 		
 		$cl = isset($_GET['cidade']) ? $_GET['cidade'] : NULL;
 		$rd = new RequisicaoDados("http://localhost/maspt/api/previsao.php", "?cl=".$cl."&tr=json&ca=".CA);	
 		$p = $pc = json_decode($rd->getResposta(), true);	
 		$pc = new PrevisaoCompleta($pc);
-		require_once("templates/pagina_web.php");		
+		
+
+		if(!is_null($tp)){
+			if($tp=="web"){
+				require_once("templates/pagina_web.php");		
+			}elseif($tp=="texto"){
+				require_once("templates/texto_formatado.php");	
+			}
+
+		}
 		
 	}
 }
