f1fe25940af0ab03e0d203c41b73dd2a642387c2
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-03-12 12:36:41 -0300

    Ajustes nos quadros de previsão semanal e estendida

--- templates/pagina_web.php
+++ templates/pagina_web.php
@@ -14,9 +14,19 @@
 		<link rel="stylesheet" type="text/css" href="static/css/weather-icons.min.css">
 		<link rel="stylesheet" type="text/css" href="static/css/weather-icons-wind.min.css">
 		<style type="text/css">		
-			.quadro{				
+			body{
+				background-color: #f4f4f4;
+			}
+			.quadro{	
+				background-color:#ededed;
 				margin-top: 5px;		
 				padding-top: 15px;	
+			}
+			.quadro *{
+				text-align: center;
+			}
+			.quadro .info {
+				color: #151515;
 			}
 			.diap{
 				text-align: center;
@@ -35,7 +45,8 @@
 				font-size: 12px;
 			}
 			footer{
-				padding: 2px;				
+				color: #696969;
+				padding: 2px;
 			}
 			footer p{
 				font-size: 12px;
@@ -48,7 +59,7 @@
 			body{font-family:'Crimison Text',Arial,sans-serif;font-size:1em}h1{font-size:24px;font-weight:bold;text-align:center;margin-bottom:25px;margin-top:20px}p{margin-bottom:10px;text-align:justify}p.red{color:#f00;text-align:center}select{padding:5px;width:100%}/*nav{margin-bottom:10px}nav ul{list-style:none}nav ul li{display:inline}*/nav a{display:inline-block;background:#333;color:white;padding:5px 15px;border:1px solid white;text-decoration:none}nav a:hover{border:1px solid #89aac0;background:#89aac0}nav a:active{background:blue}@media(max-width:480px){nav a {width:100%; padding:5px 0px;}}.center{text-align:center}.center img{margin-bottom: 10px}}			
 		</style>
 	</head>
-	<body style="background-color:#f4f4f4">	
+	<body>	
 
 		<div class="container">
 			<div class="content">
@@ -61,29 +72,36 @@
 			<div class="content quadro">
 				<?php foreach($ps as $pk => $p) { ?>
 				<div class="col2">
-					<h2><?php echo $p->getData(), "<br>", $p->getDiaSemana(); ?></h2>
-					<p><i class="sprite sprite-<?php echo $p->getIcone(); ?>" title="<?php echo $p->getDescricao(); ?>"></i></p>	
-					<p><span class="temp min"><?php echo $p->getMinima(); ?></span> <span class="celsius">ºC</span> <span class="temp max"><?php echo $p->getMaxima(); ?></span> <span class="celsius">ºC</span></p>				
-										
-					<p><i class="wi wi-umbrella" title="Probabilidade de Chuva"></i> <span class="dnone">Probabilidade de Chuva:</span> <?php echo $p->getProbChuva(); ?></p>
-					<p><i class="wi wi-sunrise" title="Nascer do Sol"></i> <span class="dnone">Nascer do Sol:</span> <?php echo $p->getNascerSol(); ?> <i class="wi wi-sunset" title="Pôr do Sol"></i> <span class="dnone">Pôr do Sol:</span> <?php echo $p->getPorSol(); ?></p>
-					<p><i class="wi wi-hot" title="Índice Ultravioleta"></i> <span class="dnone">Índice Ultravioleta:</span> <?php echo $p->getUv(); ?></p>
+					<div class="topo">
+						<h2><?php echo $p->getData(), "<br>", $p->getDiaSemana(); ?></h2>
+						<p><i class="sprite sprite-<?php echo $p->getIcone(); ?>" title="<?php echo $p->getDescricao(); ?>"></i></p>	
+						<p><span class="temp min"><?php echo $p->getMinima(); ?></span> <span class="celsius">ºC</span> <span class="temp max"><?php echo $p->getMaxima(); ?></span> <span class="celsius">ºC</span></p>				
+					</div>	
+					<div class="info">
+						<p><i class="wi wi-umbrella" title="Probabilidade de Chuva"></i> <span class="dnone">Probabilidade de Chuva:</span> <?php echo $p->getProbChuva(); ?></p>
+						<p><i class="wi wi-sunrise" title="Nascer do Sol"></i> <span class="dnone">Nascer do Sol:</span> <?php echo $p->getNascerSol(); ?> <i class="wi wi-sunset" title="Pôr do Sol"></i> <span class="dnone">Pôr do Sol:</span> <?php echo $p->getPorSol(); ?></p>
+						<p><i class="wi wi-hot" title="Índice Ultravioleta"></i> <span class="dnone">Índice Ultravioleta:</span> <?php echo $p->getUv(); ?></p>
+					</div>
 				</div>
 				<?php } ?>
 			</div>
 			<div class="content quadro">
 				<?php $i = 0; foreach($pe as $p) { $i++; ?>
 				<div class="col2">
-					<h2><?php echo $p->getData(), "<br>", $p->getDiaSemana(); ?></h2>
-					<p><i class="sprite sprite-<?php echo $p->getIcone(); ?>" title="<?php echo $p->getDescricao(); ?>"></i></p>	
-					<p><span class="temp min"><?php echo $p->getMinima(); ?></span> <span class="celsius">ºC</span> <span class="temp max"><?php echo $p->getMaxima(); ?></span> <span class="celsius">ºC</span></p>									
-					<p><i class="wi wi-umbrella" title="Probabilidade de Chuva"></i> <span class="dnone">Probabilidade de Chuva:</span> <?php echo $p->getProbChuva(); ?></p>
-					<p><i class="wi wi-sunrise" title="Nascer do Sol"></i> <span class="dnone">Nascer do Sol:</span> <?php echo $p->getNascerSol(); ?> <i class="wi wi-sunset" title="Pôr do Sol"></i> <span class="dnone">Pôr do Sol:</span> <?php echo $p->getPorSol(); ?></p>
+					<div class="topo">
+						<h2><?php echo $p->getData(), "<br>", $p->getDiaSemana(); ?></h2>
+						<p><i class="sprite sprite-<?php echo $p->getIcone(); ?>" title="<?php echo $p->getDescricao(); ?>"></i></p>	
+						<p><span class="temp min"><?php echo $p->getMinima(); ?></span> <span class="celsius">ºC</span> <span class="temp max"><?php echo $p->getMaxima(); ?></span> <span class="celsius">ºC</span></p>			
+					</div>
+					<div class="info">
+						<p><i class="wi wi-umbrella" title="Probabilidade de Chuva"></i> <span class="dnone">Probabilidade de Chuva:</span> <?php echo $p->getProbChuva(); ?></p>
+						<p><i class="wi wi-sunrise" title="Nascer do Sol"></i> <span class="dnone">Nascer do Sol:</span> <?php echo $p->getNascerSol(); ?> <i class="wi wi-sunset" title="Pôr do Sol"></i> <span class="dnone">Pôr do Sol:</span> <?php echo $p->getPorSol(); ?></p>
+					</div>
 				</div>
 				<?php if($i==6){break;}} ?>
 			</div>
 		</div>
-		<footer>
+		<footer style="margin-top:25px;">
 			<p>Mudar para visualização de <a href="index.php?cidade=<?php echo $_GET['cidade']; ?>&pagina=texto">página em texto</a></p>	
 			<p>Última atualização em <?php echo $pc->getAtualizacao(); ?> | Fonte: <a href="http://www.cptec.inpe.br/" title="Abrir página inicial do CPTEC/INPE" target="_blank">CPTEC/INPE</a><br>
 			Não nos responsabilizamos pelo uso das informações fornecidas!</p>					
