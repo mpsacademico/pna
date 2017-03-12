b8042b12501e7458aeec5c1eae3037fc87b43d25
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-03-12 12:58:36 -0300

    Formatação da estrutura da página inicial

--- templates/erro_generico.php
+++ templates/erro_generico.php
@@ -3,27 +3,25 @@
 	<head>
 	 	<meta charset="UTF-8">
 		<title>Ops! Algo de Errado Aconteceu!</title>	
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<style type="text/css">		
-		<?php
-		include_once("static/css/responsiveboilerplate234.min.css");			
-		?>			
-		body{font-family:'Crimison Text',Arial,sans-serif;font-size:1em}h1{font-size:24px;font-weight:bold;text-align:center;margin-bottom:25px;margin-top:20px}p{margin-bottom:10px;text-align:justify}p.red{color:#f00;text-align:center}select{padding:5px;width:100%}/*nav{margin-bottom:10px}nav ul{list-style:none}nav ul li{display:inline}*/nav a{display:inline-block;background:#333;color:white;padding:5px 15px;border:1px solid white;text-decoration:none}nav a:hover{border:1px solid #89aac0;background:#89aac0}nav a:active{background:blue}@media(max-width:480px){nav a {width:100%; padding:5px 0px;}}.center{text-align:center}.center img{margin-bottom: 10px}}			
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+		<link rel="icon" type="image/png" href="static/img/favicon.png">
+		<style type="text/css">
+			#centro {
+				width:300px;
+				height:300px;
+				position:absolute;
+				top:50%;
+				left:50%;
+				margin-top:-150px;
+				margin-left:-150px;
+			}
 		</style>
 	</head>
-	<body style="background-color:#f4f4f4">	
-		<div class="container">
-			<div class="content">
-				<div class="col3"></div>
-				<div class="col6">
-					<header>
-						<h1>Ops! Algo de Errado Aconteceu!</h1>
-					</header>
-					<p><strong><?php echo $erros[0]; ?></strong></p>
-					<footer>				
-					</footer>
-				</div>
-			</div>
+	<body style="background-color:#f4f4f4;font-family:sans-serif;">	
+		<div id="centro">
+			<h1>Ops! Algo de errado aconteceu!</h1>
+			<p><strong><?php echo $erros[0]; ?></strong></p>
+			<a href="/pna/">Ir para a página inicial</a>			
 		</div>
 	<!-- ANALYTICS -->
 	</body>
--- templates/pagina_inicial.php
+++ templates/pagina_inicial.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 	 	<meta charset="UTF-8">
-		<title>Página Inicial</title>	
+		<title>Previsão em Nova Aba | Início</title>	
 		<meta property="og:site_name" content="CPTEC/INPE" />
 		<meta property="og:locale" content="pt-BR" />
 	
@@ -19,19 +19,25 @@
 		<meta property="og:image:height" content="300" />	
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="icon" type="image/png" href="static/img/favicon.png">
-		<link rel="stylesheet" type="text/css" href="static/css/spritesheet.css">
 		<style type="text/css">		
-		
-		<?php
-		include_once("static/css/normalize500.min.css");	
-		include_once("static/css/skeleton204.min.css");		
-		?>		
-		
+		<?php include_once("static/css/responsiveboilerplate234.min.css"); ?>		
+			body{font-family:'Crimison Text',Arial,sans-serif;font-size:1em}h1{font-size:24px;font-weight:bold;text-align:center;margin-bottom:25px;margin-top:20px}p{margin-bottom:10px;text-align:justify}p.red{color:#f00;text-align:center}select{padding:5px;width:100%}/*nav{margin-bottom:10px}nav ul{list-style:none}nav ul li{display:inline}*/nav a{display:inline-block;background:#333;color:white;padding:5px 15px;border:1px solid white;text-decoration:none}nav a:hover{border:1px solid #89aac0;background:#89aac0}nav a:active{background:blue}@media(max-width:480px){nav a {width:100%; padding:5px 0px;}}.center{text-align:center}.center img{margin-bottom: 10px}}			
 		</style>
 	</head>
 	<body style="background-color:#f4f4f4">	
-		<h1><img src="/pna/static/img/logotipo.png" width="100px" height="100px"></h1>
-		<h2>Bem-vindo a Previsão em Nova Aba!</h2>
+		<div class="container">
+			<div class="content">
+				<div class="col2">
+					&nbsp;
+				</div>
+				<div class="col8">
+					<h1><img src="/pna/static/img/logotipo.png" width="100px" height="100px"></h1>
+					<center><h2>Bem-vindo ao Previsão em Nova Aba!</h2>
+					<a href="/pna/index.php?cidade=244">Previsão em São Paulo
+					</center>					
+				</div>
+			</div>
+		</div>
 	<!-- ANALYTICS -->
 	</body>
 </html>
