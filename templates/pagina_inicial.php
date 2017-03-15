f7a96e5223c867c8c003d8255da86b77a84621ab
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-03-15 15:31:49 -0300

    Formatação CSS da busca de cidades

--- templates/pagina_inicial.php
+++ templates/pagina_inicial.php
@@ -19,34 +19,66 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="icon" type="image/png" href="static/img/favicon.png">		
 		<style type="text/css">		
-		<?php include_once("static/css/responsiveboilerplate234.min.css"); ?>			
+		<?php include_once("static/css/responsiveboilerplate234.min.css"); ?>
+		body {
+			padding: 15px;
+		}
+		a {
+			color: 	#4682B4;
+			font-weight: bold;
+			text-decoration: none;
+		}
+		#pesquisar {			
+		}
+		footer {
+			margin-top: 5px;
+		}
+		footer p{
+			font-size: 12px;
+			text-align: center;
+		}
+		.tptexto p{
+			color: #696969;
+			padding: 2px;
+			font-size: 12px;
+			text-align: center;
+		}
 		</style>
 		<link rel="stylesheet" type="text/css" href="static/css/pna.css">
 	</head>
-	<body style="background-color:#f4f4f4">	
+	<body>
 		<div class="container">
 			<div class="content">
 				<div class="col4">
 					&nbsp;
 				</div>
-				<div class="col4">
+				<div class="col4" style="background-color:#f4f4f4">
 					<h1><img src="/pna/static/img/logotipo.png" width="100px" height="100px"></h1>
 					<center><h2>Bem-vindo ao Previsão em Nova Aba!</h2>					
 					<form method="GET" action="">
 						<input type="text" name="busca" value="<?php if(isset($d['q'])) echo $d['q']; ?>" placeholder="Busque sua cidade aqui">
-						<input type="submit" value="Pesquisar">
+						<input id="pesquisar" type="submit" value="Pesquisar">
 					</form><br>
-					</center>	
+					</center>
 					<?php 
-					if(!is_null($d)){
+					if(!is_null($d)){ ?>
+					<div style="background-color:#FFFFE0;padding:10px">
+					<?php	
+						if(count($d['c'])==0){
+							echo "<p style=\"color:#8B4513	\">Nenhum resultado encontrado</p>";
+						}				
 						foreach($d['c'] as $c){
 							echo '<p><a href="/pna/index.php?cidade='.$c['regiao_cptec'].'">'.$c['nome'].' - '.$c['uf'].'</a></p>';
-						}
-					}
-					?>			
-				</div>
+						}					
+					?>	
+					</div>		
+					<?php } ?>							
+				</div>				
 			</div>
 		</div>
+		<footer>
+			<p>&copy; <?php echo date('Y'); ?> PNA &bull; Previsão em Nova Aba</p>
+		</footer>	
 	<!-- ANALYTICS -->
 	</body>
 </html>
